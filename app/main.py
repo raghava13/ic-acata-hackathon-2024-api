4707c48636beb734cbe6e5c1754cbde8f56c25ce
Raghava B <raghava.b@integraconnect.com>
2024-04-17 09:36:27 +0530

    INITIAL

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from azure.identity import DefaultAzureCredential
+from fastapi import FastAPI
+from openai import AzureOpenAI
+
+app = FastAPI()
+
+# credential = InteractiveBrowserCredential()
+credential = DefaultAzureCredential()
+token = credential.get_token("https://cognitiveservices.azure.com/.default")
+
+
+client = AzureOpenAI(
+    api_key=token.token,
+    api_version="2023-05-15",
+    azure_endpoint="https://ic-ent-e2-cw-oai-dev.openai.azure.com/",
+)
+
+conversation = [{"role": "system", "content": "You are a helpful assistant"}]
+
+
+@app.get("/{user_input}")
+def read_root(user_input: str):
+    conversation.append({"role": "user", "content": user_input})
+
+    response = client.chat.completions.create(
+        model="dev-gpt4-32k-cpl", messages=conversation
+    )
+    return response.choices[0].message.content
